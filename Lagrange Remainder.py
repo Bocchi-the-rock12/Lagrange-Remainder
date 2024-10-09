d08dcaae7dbce455d0453d9a878c4d3f4a4caf4a
HP <afonso.canibulas@gmail.com>
2024-10-09 18:29:53 +0100

    inicio

--- Lagrange Remainder.py
+++ Lagrange Remainder.py
@@ -1,2 +1,34 @@
-import matplotlib
-from sympy import symbols
+import matplotlib.pyplot as plt
+from sympy import symbols, simplify
+from numpy import linspace
+
+
+def l_func(x_values, k, n):
+   l = 1
+   x = symbols("x")
+   for i in range(n + 1):
+       if i != k:
+           # L parameters
+           l *= (x - x_values[i]) / (x_values[k] - x_values[i])
+   return l
+
+
+def polynomial_interpolation(x_var, y, z):
+   p = 0
+   for k in range(z + 1):
+       # Lagrange Polynomial
+       p += y[k] * l_func(x_var, k, z)
+   return p
+
+
+def main():
+   x_data_set = []
+   y_data_set = []
+   print("Lagrange Polynomial Remainder Error")
+   n = int(input("> "))
+   for z in range(n + 1):
+       # Get x, y data from user
+       xi, yi = map(int, input("(x, y): ").split(","))
+       x_data_set.append(xi)
+       y_data_set.append(yi)
+main()
