4a5c6ccc9079cc1a35694422ce0f5cb270569f64
Afonso <afonso.canibulas@gmail.com>
2024-10-13 15:03:38 +0100

    olá

--- Lagrange Remainder.py
+++ Lagrange Remainder.py
@@ -1,47 +1,105 @@
 import matplotlib.pyplot as plt
 import sympy as sp
 from numpy import linspace
-import random
-
-def l_func(x_values, k, degree):
-   l = 1
-   x = sp.symbols("x")
-   for i in range(degree + 1):
-       if i != k:
-           # L parameters
-           l *= (x - x_values[i]) / (x_values[k] - x_values[i])
-   return l
+import numdifftools as nd
 
 
-def polynomial_interpolation(x_var, y, degree):
-   p = 0
-   for k in range(degree + 1):
-       # Lagrange Polynomial
-       p += y[k] * l_func(x_var, k, degree)
-   return sp.simplify(p)
-
-
-def Lagrange_error_formula(x_var, y_var, degree):
+def l_func(x_values, k):
+    l = 1
+    degree = len(x_values) - 1
     x = sp.symbols("x")
-    error_formula = 1
-    for b in range(degree + 1):
-        derivative = sp.diff(polynomial_interpolation(x_var, y_var, degree), x, degree)
-        error_formula *= derivative * random.choice(x_var)
-    error_formula / sp.factorial(degree - 1)
-    print(error_formula)
-    return error_formula
+    # Calculation of l node polynomials
+    for i in range(degree + 1):
+        if i != k:
+            l *= (x - x_values[i]) / (x_values[k] - x_values[i])
+    return l
+
+
+def polynomial_interpolation(x, y):
+    p = 0
+    degree = len(x) - 1
+    for k in range(degree + 1):
+        # Lagrange Polynomial calculation
+        p += y[k] * l_func(x, k)
+    return p
+
+
+def Lagrange_remainder(function, x_var):
+    x = sp.symbols("x")
+    degree = len(x_var) - 1
+    fact = sp.factorial(degree + 1)
+    error_expression = 0
+    numeric_function = sp.lambdify(x, function, "numpy")
+    for m in range(degree + 1):
+        product = 1
+        for i in range(degree + 1):
+            if i != m:
+                product *= (x - x_var[i])
+        point_derivative = nd.Derivative(numeric_function)(x_var[m])
+        error_expression += (point_derivative / fact) * product
+    return error_expression
+
+
+def graph_plot(x, y_data, polynomial, remainder):
+    plt.figure("Polynomial Interpolation", figsize=(11, 8))
+    plt.grid()
+    plt.xlabel("X-axis", fontsize=12)
+    plt.ylabel("Y-axis", fontsize=12)
+    plt.title("Polynomial Interpolation with Lagrange Remainder", fontsize=20)
+    plt.axhline(0, color="black", linewidth=2, ls="-")
+    plt.axvline(0, color="black", linewidth=2, ls="-")
+
+    # x values for plotting
+    x_values = linspace(min(x), max(x), 750)
+
+    # Calculate y values for the polynomial and the remainder
+    y_values_poly = [polynomial.subs("x", val) for val in x_values]
+    y_values_rem = [remainder.subs("x", val) for val in x_values]
+
+    # Convert expressions into floats
+    y_values_poly = [float(val) for val in y_values_poly]
+    y_values_rem = [float(val) for val in y_values_rem]
+
+    # Plot limits
+    x_min, x_max = min(x_values), max(x_values)
+    y_min = min(min(y_values_poly), min(y_values_rem))
+    y_max = max(max(y_values_poly), max(y_values_rem))
+    x_padding = (x_max - x_min) * 0.1
+    y_padding = (y_max - y_min) * 0.1
+    plt.xlim(x_min - x_padding, x_max + x_padding)
+    plt.ylim(y_min - y_padding, y_max + y_padding)
+
+    # Graph plot
+    plt.plot(x_values, y_values_poly, label='Interpolating Polynomial', color='blue', linestyle='-')
+    plt.plot(x_values, y_values_rem, label='Lagrange Remainder', color='red', linestyle='--')
+
+    plt.gcf().canvas.manager.toolbar.zoom()
+    plt.legend()
+    plt.show()
 
 
 def main():
-    x_data_set = []
-    y_data_set = []
-    print("Lagrange Polynomial Remainder Error")
-    n = int(input("> "))
-    for z in range(n + 1):
-        # Get x, y data from user
-        xi, yi = map(int, input("(x, y): ").split(","))
-        x_data_set.append(xi)
-        y_data_set.append(yi)
-    Lagrange_error_formula(x_data_set, y_data_set, n)
+    x = sp.symbols("x")
+    function_input = input("Insert a function in terms of x (e.g 2x^3+ 3x^2 - 4x + 3): ")
+    # Verification for invalid functions
+    try:
+        function = sp.sympify(function_input)
+    except Exception as e:
+        print(f"Invalid function! {e}")
+        return
+    print(f"Function accepted: f(x) = {function}")
+    # Gather x, y data from user
+    data_points = int(input("Number of points of interpolated function: "))
+    x_data = []
+    y_data = []
+    for x in range(data_points):
+        x_var = float(input(f"x[{x}]: "))
+        x_data.append(x_var)
+        y_var = function.subs(x, x_var)
+        y_data.append(y_var)
+    polynomial = polynomial_interpolation(x_data, y_data)
+    error_expression = Lagrange_remainder(function, x_data)
+    graph_plot(x_data, y_data, polynomial, error_expression)
+
 
 main()
